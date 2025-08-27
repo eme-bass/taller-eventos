10de1c6efb92d6523a71ddf9466def74164e0161
Emely <emelygonzalez018@gmail.com>
2025-08-27 00:16:46 -0300

    agrego alerta al presionar el div y estilos a la página

---
+++ eventos.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function(){
+    const div = document.querySelector("div");
+    
+    div.addEventListener('click', function(){
+        alert("Hola, soy el div!");
+    })
+})
